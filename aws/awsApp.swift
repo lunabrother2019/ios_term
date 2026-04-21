8cbf1c379f11608962ec3d93c49b4ac547f33113
killinux <killinux@163.com>
2026-04-21 16:24:04 +0800

    Initial Commit

---
+++ aws/awsApp.swift
@@ -0,0 +1,17 @@
+//
+//  awsApp.swift
+//  aws
+//
+//  Created by ByteDance on 2026/4/21.
+//
+
+import SwiftUI
+
+@main
+struct awsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
